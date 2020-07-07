b0ec3bbb0e50aef91542c64601c01d216096b1cf
Lanqin <fyempathy@gmail.com>
2020-07-07 14:54:30 +1200

    modified the grammatical mistakes in the comment

--- SCRIPTS/tgautil.py
+++ SCRIPTS/tgautil.py
@@ -1,11 +1,11 @@
 '''
 Copyright 2020 Electronic Arts Inc.
 
-This program is is free software: you can redistribute it and/or modify it under the terms of 
+This program is free software: you can redistribute it and/or modify it under the terms of 
 the GNU General Public License as published by the Free Software Foundation, 
 either version 3 of the License, or (at your option) any later version.
 
-This program is is distributed in the hope that it will be useful, but with permitted additional restrictions 
+This program is distributed in the hope that it will be useful, but with permitted additional restrictions 
 under Section 7 of the GPL. See the GNU General Public License in LICENSE.TXT 
 distributed with this program. You should have received a copy of the 
 GNU General Public License along with permitted additional restrictions 
